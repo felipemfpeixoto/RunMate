cc0d4c4ec14d10bfdf180b998d07b365af52bba4
Felipe <felipe.m.f.peixoto@gmail.com>
2024-05-02 15:23:53 -0300

    adicionando mais arquivos =)

---
+++ RunMate/Elements/BotaoDia.swift
@@ -0,0 +1,18 @@
+//
+//  BotaoDia.swift
+//  RunMate
+//
+//  Created by infra on 02/05/24.
+//
+
+import SwiftUI
+
+struct BotaoDia: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BotaoDia()
+}
---
+++ RunMate/Elements/SemanaRoadMap.swift
@@ -0,0 +1,18 @@
+//
+//  SemanaRoadMap.swift
+//  RunMate
+//
+//  Created by infra on 02/05/24.
+//
+
+import SwiftUI
+
+struct SemanaRoadMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SemanaRoadMap()
+}
