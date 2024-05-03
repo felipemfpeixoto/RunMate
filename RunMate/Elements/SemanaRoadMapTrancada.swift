b2dc4571537db41bdcb6ba4f7c3700cf27661f12
robertacordeirosampf <robertacordeirosampf@gmail.com>
2024-05-03 14:50:58 -0300

    Roadmap com cadeado e medalha feitos

--- RunMate/Elements/SemanaRoadMapTrancada.swift
+++ RunMate/Elements/SemanaRoadMapTrancada.swift
@@ -1,26 +1,26 @@
 //
-//  SemanaRoadMap.swift
+//  SemanaRoadMapTrancada.swift
 //  RunMate
 //
-//  Created by infra on 02/05/24.
+//  Created by Roberta Cordeiro on 03/05/24.
 //
 
 import SwiftUI
 
-struct SemanaRoadMap: View {
+struct SemanaRoadMapTrancada: View {
     var body: some View {
         HStack(spacing: 0) {
             ZStack(alignment: .leading) {
                 Rectangle()
-                    .foregroundColor(.turquoiseGreen)
+                    .foregroundColor(.blackBlue)
                     .frame(width: 200, height: 70)
                     .cornerRadius(18)
                     .overlay(
                         RoundedRectangle(cornerRadius: 18)
-                            .stroke(Color.lilacPurple, lineWidth: 4)
+                            .stroke(Color.turquoiseGreen, lineWidth: 4)
                     )
                 Text("Semana 1")
-                    .foregroundColor(.blackBlue)
+                    .foregroundColor(.turquoiseGreen)
                     .font(.title3)
                     .fontWeight(.semibold)
                     .padding(.horizontal) // Add padding to the text
@@ -29,7 +29,7 @@ struct SemanaRoadMap: View {
                 
                 
             }
-                Image("Medalha")
+                Image("Cadeado")
                 .offset(x: -50)
       
         }
@@ -37,5 +37,5 @@ struct SemanaRoadMap: View {
 }
 
 #Preview {
-    SemanaRoadMap()
+    SemanaRoadMapTrancada()
 }
--- RunMate/Views/RoadMapView.swift
+++ RunMate/Views/RoadMapView.swift
@@ -8,10 +8,70 @@
 import SwiftUI
 
 struct RoadMapView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        var body: some View {
+                VStack {
+                    HStack {
+                        
+                        Image(systemName: "arrow.backward")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                            .fontWeight(.medium)
+                        Spacer()
+                    }
+                    .padding(.leading, 30)
+                    .padding(.bottom, 2)
+                    HStack {
+                        Text("Minha meta")
+                            .foregroundStyle(.white)
+                            .font(.title)
+                            .fontWeight(.bold)
+                        Spacer()
+                    }
+                    .padding(.leading, 30)
+                    
+                    HStack {
+                        Text("5 KM | Avançado")
+                            .foregroundStyle(.turquoiseGreen)
+                            .font(.title2)
+                            .fontWeight(.semibold)
+                        Spacer()
+                    }
+                    .padding(.leading, 30)
+                    
+                        
+                    ScrollView {
+                    VStack {
+                        ZStack {
+                            Image("Road")
+                                .resizable()
+                                .scaledToFit()
+                            
+                            
+//                            ZStack(alignment: .leading) {
+//                                Rectangle()
+//                                    .foregroundColor(.red)
+//                                    .frame(width: 200, height: 70)
+//                                    .cornerRadius(18)
+//                                    .overlay(
+//                                        RoundedRectangle(cornerRadius: 18)
+//                                            .stroke(Color.green, lineWidth: 4)
+//                                    )
+//                                
+//                                Text("Semana 1")
+//                                    .foregroundColor(.white)
+//                                    .font(.title3)
+//                                    .fontWeight(.semibold)
+//                                    .padding(.horizontal) // Add padding to the text
+//                            }
+                        }
+                    }
+                }
+            }
+            .background(.blackBlue)
+     
+        }
     }
-}
+
 
 #Preview {
     RoadMapView()
