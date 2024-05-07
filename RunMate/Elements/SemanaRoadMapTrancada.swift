2e3df0791fa5a9c0d210b84d928b96ad193c8f54
robertacordeirosampf <robertacordeirosampf@gmail.com>
2024-05-07 14:13:09 -0300

    Lógica Semanas Cadeados

--- RunMate/Elements/SemanaRoadMapTrancada.swift
+++ RunMate/Elements/SemanaRoadMapTrancada.swift
@@ -21,17 +21,30 @@ struct RoadShape: Shape {
     func path(in rect: CGRect) -> Path {
         
         Path { path in
-            if right {
-                path.move(to: .zero)
-                path.addLine(to: CGPoint(x: 0, y: rect.maxY))
-                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-            } else {
-                path.move(to: CGPoint(x: rect.maxX, y: 0))
-                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
-                path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
-                
+            path.move(to: CGPoint(x: 0, y: 100))
+            
             }
         }
         
     }
-}
+
+
+//struct RoadShape: Shape {
+//    var right: Bool
+//    func path(in rect: CGRect) -> Path {
+//        
+//        Path { path in
+//            if right {
+//                path.move(to: .zero)
+//                path.addLine(to: CGPoint(x: 0, y: rect.maxY))
+//                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//            } else {
+//                path.move(to: CGPoint(x: rect.maxX, y: 0))
+//                path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+//                path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+//                
+//            }
+//        }
+//        
+//    }
+//}
